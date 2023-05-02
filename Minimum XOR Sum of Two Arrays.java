13ad75f8efe01c367297d3dca726d309bad15aec
abinet1 <abinettassewabi@gmail.com>
2023-05-02 04:55:46 +0300

    Minimum XOR Sum of Two Arrays

---
+++ Minimum XOR Sum of Two Arrays.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i=0; i<nums.length/2; i++){
+            if(sum < (nums[i]+nums[nums.length-(i+1)])){
+                sum=nums[i]+nums[nums.length-(i+1)];
+            }
+        }
+        return sum;
+    }
+}
